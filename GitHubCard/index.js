a56043c7adba9bd42449ddec33669893387a7e60
teresafranxman97 <teresafranxman97@gmail.com>
2020-11-10 10:56:01 -0600

    attempted to create user cards with github data but nothing displays on screen or in the DOM - trying to find the issue now

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,7 @@
     https://api.github.com/users/<your name>
 */
 
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,7 +29,13 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [ 
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'
+];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +56,66 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function cardCreator (data) {
+// Creating required elements
+  const cardContainer = document.createElement('div');
+  const cardImg = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const cardTitle = document.createElement('h3');
+  const cardUser = document.createElement('p');
+  const useLocation = document.createElement('p');
+  const userPro = document.createElement('p');
+  const ahref = document.createElement('a');
+  const useFollows = document.createElement('p');
+  const useFollowing = document.createElement('p');
+  const useBio = document.createElement('p');
+
+  // Adding text to content
+  cardTitle.textContent = 'data.login'
+  cardImg.src = data.avatar_url
+  cardUser.textContent = 'username';
+  useLocation.textContent = 'location';
+  ahref.textContent = 'address to user page';
+  useFollows.textContent = 'follows count';
+  useFollowing.textContent = 'users following count';
+  useBio.textContent = 'bio;'
+
+  
+// Adding class names to my newly created elements
+  cardContainer.classList.add('card');
+  cardInfo.classList.add('card-info');
+  cardTitle.classList.add('name');
+
+
+// Appending children elements to parent element(s)
+  cardContainer.appendChild(cardImg);
+  cardContainer.appendChild(cardInfo);
+  cardInfo.appendChild(cardTitle);
+  cardInfo.appendChild(cardUser);
+  cardInfo.appendChild(useLocation);
+  cardInfo.appendChild(userPro);
+  userPro.appendChild(ahref);
+  cardInfo.appendChild(useFollows);
+  cardInfo.appendChild(useFollowing);
+  cardInfo.appendChild(useBio);
+
+  return cardContainer;
+}
+
+console.log(cardCreator())
+
+
+followersArray.forEach(username => {
+  // eslint-disable-next-line no-undef
+  axios.get(`https://api.github.com/users/${username}`)
+  .then(response => {
+    console.log(response);
+      const card = cardCreator(response.data)
+      document.querySelector('.cards').appendChild(card);
+      return card
+  })
+})
 
 /*
   List of LS Instructors Github username's:
